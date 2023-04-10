e9050dd7a684b3283973cccd420a95838512d765
Stanley Pageot <stanley@rxdu.io>
2023-04-10 14:10:55 -0700

    error catching

--- controllers/forecast.controller.js
+++ controllers/forecast.controller.js
@@ -5,16 +5,19 @@ const forecast = async (req, res) => {
   if (req.query.location) {
     const userQuery = req.query.location;
     const city = userQuery.split(",")[0];
-    const userState =
-      states[
-        userQuery.split(",")[1].replace(/\s/g, "").toUpperCase()
-      ].toLowerCase();
+    const StateAbbrev =
+      states[userQuery.split(",")[1].replace(/\s/g, "").toUpperCase()];
+    if (StateAbbrev) {
+      const userState = StateAbbrev.toLowerCase();
 
-    const forecastDetails = await fetch(
-      `https://api.openweathermap.org/data/2.5/forecast?q=${city},${userState}&appid=${process.env.WEATHER_API_KEY}`
-    ).then((response) => response.json());
+      const forecastDetails = await fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${city},${userState}&appid=${process.env.WEATHER_API_KEY}`
+      ).then((response) => response.json());
 
-    return res.json({ forecastDetails });
+      return res.status(200).json({ forecastDetails });
+    } else {
+      return res.status(404).json("State Not Found");
+    }
   } else if (req.query.zip) {
     const userZipQuery = req.query.zip;
 
@@ -22,7 +25,7 @@ const forecast = async (req, res) => {
       `https://api.openweathermap.org/data/2.5/forecast?zip=${userZipQuery}&appid=${process.env.WEATHER_API_KEY}`
     ).then((response) => response.json());
 
-    return res.json({ forecastDetails });
+    return res.status(200).json({ forecastDetails });
   } else if (req.query.state) {
     const userStateQuery = req.query.state;
 
@@ -30,7 +33,11 @@ const forecast = async (req, res) => {
       `https://api.openweathermap.org/data/2.5/forecast?q=${userStateQuery}&appid=${process.env.WEATHER_API_KEY}`
     ).then((response) => response.json());
 
-    return res.json({ forecastDetails });
+    if (forecastDetails.cod != "404") {
+      return res.status(200).json({ forecastDetails });
+    } else {
+      return res.status(404).json("City/State Not Found");
+    }
   }
 };
 
